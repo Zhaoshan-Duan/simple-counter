faee933ae154476e0a04297b38946dbd2f67bbb8
Zhaoshan-Duan <joshua.duan47@gmail.com>
2023-02-20 23:06:04 -0800

    required story completed

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.simplecounter
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.simplecounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val button = binding.button
+        val upgradeBtn = binding.upgradeBtn
+        val textView = binding.textView
+
+        count = 98
+        button.setOnClickListener {
+            count++
+            textView.text = count.toString()
+
+            if (count >= 100) {
+                upgradeBtn.visibility = View.VISIBLE
+                upgradeBtn.setOnClickListener {
+                    button.text = "Add 2"
+
+                    button.setOnClickListener {
+                        count += 2
+                        textView.text = count.toString()
+                    }
+                    upgradeBtn.visibility = View.GONE
+                }
+            }
+
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
